7a2e20fd3a5230378c603ed2da3df07741d0e4ff
s.vosse <s.vosse@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-12 10:41:06 +0000

    Removed generic classes cghRaw,cghSeg,cghCall
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CGHcall@59113 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/private.R
+++ R/private.R
@@ -19,32 +19,6 @@
     result
 }
 
-
-.makeSegments <- function(data,chrdata) {
-    #data <- segmented(x)[,5]
-#    chrdata <- chromosomes(x)
-    previous    <- 2000
-    chrpr <- -100
-    values      <- c()
-    start       <- c()
-    end         <- c()
-    el <- length(data)
-    data <- c(data,-10000) #add value to allow data[i+1]
-    for (i in 1:el) {
-        if ((data[i] != previous & previous != data[i+1]) | chrdata[i] != chrpr) { #bug repaired 12/06/09
-            start   <- c(start, i)
-            last    <- i - 1
-            if (last > 0) end <- c(end, last)
-            values  <- c(values, data[i])
-        }
-        previous    <- data[i]
-        chrpr <- chrdata[i]
-    }
-    end     <- c(end, el)
-    result  <- cbind(values, start, end)
-    result
-}
-
 .assignNames <- function(matrix, object) {
     colnames(matrix) <- sampleNames(object)
     rownames(matrix) <- featureNames(object)
